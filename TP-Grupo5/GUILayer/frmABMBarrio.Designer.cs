4b7d4a5553e316d9810018732cb2a9ee2106991b
Santidmlv <santidmlv@gmail.com>
2021-10-19 20:24:14 -0300

    abm barrios

---
+++ TP-Grupo5/GUILayer/frmABMBarrio.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace TP_Grupo5.GUILayer
+{
+    partial class frmABMBarrio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtIDBarrio = new System.Windows.Forms.TextBox();
+            this.txtNombreBarrio = new System.Windows.Forms.TextBox();
+            this.lblNombreBarrio = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(77, 25);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 15);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID:";
+            // 
+            // txtIDBarrio
+            // 
+            this.txtIDBarrio.Enabled = false;
+            this.txtIDBarrio.Location = new System.Drawing.Point(106, 17);
+            this.txtIDBarrio.Name = "txtIDBarrio";
+            this.txtIDBarrio.Size = new System.Drawing.Size(128, 23);
+            this.txtIDBarrio.TabIndex = 1;
+            // 
+            // txtNombreBarrio
+            // 
+            this.txtNombreBarrio.Location = new System.Drawing.Point(106, 55);
+            this.txtNombreBarrio.Name = "txtNombreBarrio";
+            this.txtNombreBarrio.Size = new System.Drawing.Size(128, 23);
+            this.txtNombreBarrio.TabIndex = 3;
+            // 
+            // lblNombreBarrio
+            // 
+            this.lblNombreBarrio.AutoSize = true;
+            this.lblNombreBarrio.Location = new System.Drawing.Point(12, 58);
+            this.lblNombreBarrio.Name = "lblNombreBarrio";
+            this.lblNombreBarrio.Size = new System.Drawing.Size(88, 15);
+            this.lblNombreBarrio.TabIndex = 2;
+            this.lblNombreBarrio.Text = "Nombre Barrio:";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(77, 106);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 4;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(159, 106);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmABMBarrio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 144);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtNombreBarrio);
+            this.Controls.Add(this.lblNombreBarrio);
+            this.Controls.Add(this.txtIDBarrio);
+            this.Controls.Add(this.lblID);
+            this.Name = "frmABMBarrio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtIDBarrio;
+        private System.Windows.Forms.TextBox txtNombreBarrio;
+        private System.Windows.Forms.Label lblNombreBarrio;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
